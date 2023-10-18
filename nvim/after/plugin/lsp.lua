3cb45bd68c7758eee678c9f90dcbe42c1c17909c
Javier Portillo <jportillo@zetaglobal.com>
2023-10-18 10:25:15 -0400

    initial commit

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,32 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+require'lspconfig'.ruff_lsp.setup{
+  init_options = {
+    settings = {
+      -- Any extra CLI arguments for `ruff` go here.
+      args = {},
+    }
+  }
+}
+
+require'lspconfig'.pylsp.setup{
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {'W391'},
+          maxLineLength = 88
+        }
+      }
+    }
+  }
+}
+
+lsp.setup()
